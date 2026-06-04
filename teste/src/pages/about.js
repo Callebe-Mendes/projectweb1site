ef95c28cfcdf7e9fb243db64db6b59a09745d825
Gustavo Kenji Nunes Tsukioka <gustavokntsu@gmail.com>
2026-06-04 20:58:45 +0000

    feat: páginas Sobre e Contato estilizadas

--- teste/src/pages/about.js
+++ teste/src/pages/about.js
@@ -1,25 +1,15 @@
 import Footer from "@/components/Footer";
 import Menu from "@/components/Menu";
-import Author from "@/components/Author";
-import { useState } from "react";
+import AboutContent from "@/components/AboutContent";
 
 function About() {
-  const [info, setInfo] = useState("Aula de Web I!");
-
   return (
     <main>
-      <Menu/>
-      <h1>Sobre a Aula de Web I</h1>
-      
-      {/* Aqui estamos usando as Props e o Children do componente Author */}
-      <Author name={"Adriano Baião"} courses={{price: 50, workload:"60 horas"}}>Web I</Author>
-      
-      {/* Aqui é o teste do useState */}
-      <p>{info}</p>
-      <button onClick={()=> setInfo("Hoje é sexta-feiraaaa!")}>Alterar</button>
-      
-      <Footer/>
+      <Menu />
+      <AboutContent />
+      <Footer />
     </main>
-  )
+  );
 }
+
 export default About;
--- teste/src/pages/contact.js
+++ teste/src/pages/contact.js
@@ -1,45 +1,18 @@
+
 import Footer from "@/components/Footer";
 import Menu from "@/components/Menu";
-import { useState } from "react";
+import ContactContent from "@/components/ContactContent";
 
-// Exemplo de uma Arrow Function
 const Contact = () => {
-  // useState para guardar os dados do formulário
-  const [data, setData] = useState({
-    nameUser: "",
-    emailUser: ""
-  });
-
-  // Função para capturar o que é digitado nos inputs
-  const valueInput = e => setData({...data, [e.target.name]: e.target.value});
-
-  // Função disparada ao enviar o formulário
-  const addUser = (e) => {
-    e.preventDefault(); // Evita que a página recarregue
-    console.log("Nome: " + data.nameUser);
-    console.log("E-mail: " + data.emailUser);
-  }
-
   return (
     <>
       <main>
-        <Menu/><br/><br/>
-        <h2>Vem falar com a gente!</h2>
-        
-        <form onSubmit={addUser}>
-          <label>Nome: </label>
-          <input type="text" name="nameUser" placeholder="Nome do Cliente" onChange={valueInput} /><br/> 
-          
-          <label>Email: </label>
-          <input type="email" name="emailUser" placeholder="E-mail do Cliente" onChange={valueInput} /><br/>
-          
-          <button type="submit">Cadastrar</button>
-        </form>
-        <br/><br/>
-        <Footer/>
+        <Menu />
+        <ContactContent />
+        <Footer />
       </main>
     </>
-  )
-}
+  );
+};
 
 export default Contact;
