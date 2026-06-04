5813d5fbf100e2ee9121ea54f2f935305c0dcc3e
Gustavo Kenji Nunes Tsukioka <gustavokntsu@gmail.com>
2026-06-04 20:57:20 +0000

    feat: página de detalhes do produto com useState

--- teste/src/pages/produto/[id].js
+++ teste/src/pages/produto/[id].js
@@ -1,9 +1,7 @@
 import Menu from "@/components/Menu";
 import Footer from "@/components/Footer";
 import { useRouter } from "next/router";
-
-// Quando o Integrante 3 criar a lógica e interface de detalhes, vocês descomentam:
-// import ProdutoDetalheContent from "@/components/ProdutoDetalheContent";
+import ProdutoDetalheContent from "@/components/ProdutoDetalheContent";
 
 export default function ProdutoDetalhe() {
   const router = useRouter();
@@ -12,11 +10,8 @@ export default function ProdutoDetalhe() {
   return (
     <>
       <Menu />
-      <main style={{ padding: '20px', minHeight: '60vh' }}>
-        <h2>Detalhes do Produto ID: {id}</h2>
-        <p>A lógica do useState e os dados simulados do produto vão carregar aqui!</p>
-        
-        {/* <ProdutoDetalheContent id={id} /> */}
+      <main style={{ minHeight: '60vh' }}>
+        <ProdutoDetalheContent id={id} />
       </main>
       <Footer />
     </>
