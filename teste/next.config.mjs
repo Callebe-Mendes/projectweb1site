76a14626c265ed7fccfa883d2926587074046675
GustavoKntsu <gustavokntsu@gmail.com>
2026-06-11 19:20:26 -0300

    Apagando duplicatas

--- teste/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
-  reactStrictMode: true,
-  allowedDevOrigins: ['127.0.0.1'],
-};
-
-export default nextConfig;
